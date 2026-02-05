c18370ccd19b9f88ce655e1b4e9c0ec7d444a43d
Khadija Daruwala <khadija.genius@gmail.com>
2026-02-05 11:38:37 -0500

    Added constants file for strings

---
+++ MotionVault/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  MotionVault
+//
+//  Created by Khadija Daruwala on 2026-02-05.
+//
+
+import Foundation
+
+struct Constants{
+    static let homeString = "Home"
+    static let searchString = "Search"
+    static let upcomingString = "Upcoming"
+    static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let searchIconString = "magnifyingglass"
+    static let upcomingIconString = "play.circle"
+    static let downloadIconString = "arrow.down.to.line"
+}
